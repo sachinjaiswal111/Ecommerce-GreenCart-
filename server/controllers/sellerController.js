0481199810afa321f4e1a85acd63bdf4dc39f6a0
Sachin <tryy.sachin@gmail.com>
2025-07-01 21:22:03 +0530

    correct seller login

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -8,7 +8,7 @@ export const sellerLogin = async (req, res) => {
     try{
 const { email, password } = req.body;
 
-    if (password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIl)
+    if (password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL)
  {
         const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
